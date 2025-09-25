87a21a9974015b6c94bdc7e29f3bbb6a8279591f
Logvin <Logvinetsd@mail.ru>
2025-09-25 16:43:41 +0700

    Убран глобальный вектор vector<Artefact*> container

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,7 +1,9 @@
 ﻿#include <iostream>
 #include <vector>
 #include <string>
-#include <algorithm> // Для std::remove_if
+#include <algorithm>
+#include <memory>
+#include <limits> // для numeric_limits
 using namespace std;
 
 // Базовый класс "Артефакт"
@@ -12,7 +14,6 @@ public:
 
     Artefact(const string& c, int y) : content(c), year(y) {}
 
-    // Виртуальная функция для вывода информации
     virtual void print() const {
         cout << "Содержание: " << content << " | Год: " << year << endl;
     }
@@ -45,123 +46,121 @@ public:
 };
 
 // Контейнер для хранения артефактов
-vector<Artefact*> container;
+class ArtefactContainer {
+private:
+    vector<unique_ptr<Artefact>> container;
+
+public:
+    void addObject(unique_ptr<Artefact> obj) {
+        container.push_back(move(obj));
+    }
+
+    void printContainer() const {
+        if (container.empty()) {
+            cout << "Контейнер пуст!" << endl;
+        }
+        else {
+            for (const auto& obj : container) {
+                obj->print();
+            }
+        }
+    }
+
+    void removeObject() {
+        cout << "Выберите поле для удаления объектов:\n";
+        cout << "1 - Содержание\n2 - Автор (только афоризмы)\n3 - Страна (только пословицы)\n4 - Год\n";
+
+        int choice;
+        cin >> choice;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // очищаем буфер
+
+        string keyword;
+        int yearKey = 0;
+        if (choice == 4) {
+            cout << "Введите год: ";
+            cin >> yearKey;
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+        else {
+            cout << "Введите ключевое слово: ";
+            getline(cin, keyword);
+        }
+
+        container.erase(
+            remove_if(container.begin(), container.end(),
+                [&](const unique_ptr<Artefact>& obj) {
+                    switch (choice) {
+                    case 1: return obj->content.find(keyword) != string::npos;
+                    case 2:
+                        if (Aphorism* a = dynamic_cast<Aphorism*>(obj.get()))
+                            return a->author.find(keyword) != string::npos;
+                        return false;
+                    case 3:
+                        if (Proverb* p = dynamic_cast<Proverb*>(obj.get()))
+                            return p->country.find(keyword) != string::npos;
+                        return false;
+                    case 4: return obj->year == yearKey;
+                    default:
+                        cout << "Неверный выбор поля!" << endl;
+                        return false;
+                    }
+                }),
+            container.end());
+    }
+};
 
 // Функция для добавления объекта
-void addObject() {
-    cout << "Выберите тип объекта: 1 - Афоризм, 2 - Пословица: ";
+void addObject(ArtefactContainer& artefactContainer) {
     int type;
+    cout << "Выберите тип объекта: 1 - Афоризм, 2 - Пословица: ";
     cin >> type;
-    cin.ignore(); // очистка буфера
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-    string content, extra;
-    int year;
-
+    string content;
     cout << "Введите содержание: ";
     getline(cin, content);
 
+    int year;
     cout << "Введите год создания: ";
     cin >> year;
-    cin.ignore();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
     if (type == 1) {
+        string author;
         cout << "Введите автора: ";
-        getline(cin, extra);
-        container.push_back(new Aphorism(content, extra, year));
+        getline(cin, author);
+        artefactContainer.addObject(make_unique<Aphorism>(content, author, year));
     }
     else if (type == 2) {
+        string country;
         cout << "Введите страну: ";
-        getline(cin, extra);
-        container.push_back(new Proverb(content, extra, year));
+        getline(cin, country);
+        artefactContainer.addObject(make_unique<Proverb>(content, country, year));
     }
     else {
         cout << "Неверный тип объекта!" << endl;
     }
 }
 
-// Функция для удаления объекта
-void removeObject() {
-    cout << "Выберите поле для удаления объектов:\n";
-    cout << "1 - Содержание\n2 - Автор (только афоризмы)\n3 - Страна (только пословицы)\n4 - Год\n";
-    int choice;
-    cin >> choice;
-    cin.ignore();
-
-    string keyword;
-    int yearKey = 0;
-    if (choice == 4) {
-        cout << "Введите год: ";
-        cin >> yearKey;
-        cin.ignore();
-    }   
-    else {
-        cout << "Введите ключевое слово: ";
-        getline(cin, keyword);
-    }
-
-    container.erase(
-        remove_if(container.begin(), container.end(),
-            [&](Artefact* obj) {
-                bool toDelete = false;
-                switch (choice) {
-                case 1:
-                    if (obj->content.find(keyword) != string::npos) toDelete = true;
-                    break;
-                case 2:
-                    if (Aphorism* a = dynamic_cast<Aphorism*>(obj)) {
-                        if (a->author.find(keyword) != string::npos) toDelete = true;
-                    }
-                    break;
-                case 3:
-                    if (Proverb* p = dynamic_cast<Proverb*>(obj)) {
-                        if (p->country.find(keyword) != string::npos) toDelete = true;
-                    }
-                    break;
-                case 4:
-                    if (obj->year == yearKey) toDelete = true;
-                    break;
-                default:
-                    cout << "Неверный выбор поля!" << endl;
-                    break;
-                }
-                if (toDelete) delete obj;
-                return toDelete;
-            }),
-        container.end());
-}
-
-// Функция для вывода контейнера
-void printContainer() {
-    if (container.empty()) {
-        cout << "Контейнер пуст!" << endl;
-    }
-    else {
-        for (auto obj : container) {
-            obj->print();
-        }
-    }
-}
-
+// Главная функция
 int main() {
     setlocale(0, "");
-    string command;
+    ArtefactContainer artefactContainer;
 
     cout << "Программа Кладезь мудрости\n";
     cout << "Доступные команды: ADD, REM, PRINT, EXIT\n";
 
+    string command;
     while (true) {
         cout << "\nВведите команду: ";
-        cin >> command;
+        getline(cin, command); // теперь корректно читаем команду
 
-        if (command == "ADD") addObject();
-        else if (command == "REM") removeObject();
-        else if (command == "PRINT") printContainer();
+        if (command == "ADD") addObject(artefactContainer);
+        else if (command == "REM") artefactContainer.removeObject();
+        else if (command == "PRINT") artefactContainer.printContainer();
         else if (command == "EXIT") break;
         else cout << "Неизвестная команда!" << endl;
     }
-
-    // Очистка памяти
-    for (auto obj : container) delete obj;
 
     return 0;
 }
